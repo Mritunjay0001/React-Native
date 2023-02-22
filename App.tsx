25ede31dad91e3314ccfc22f9af6a2db9b535f89
Mritunjay Kumar <akshaysingh906022@gmail.com>
2023-02-23 01:32:35 +0530

    we can use html

--- App.tsx
+++ App.tsx
@@ -21,11 +21,17 @@ function App(): JSX.Element {
       {/* this is the first thing ---------> */}
 
 
-<Text style={{fontSize:30}}>akshay</Text>
+{/* <Text style={{fontSize:30}}>akshay</Text>
 <Text style={{fontSize:30}}>singh</Text>
 <Text style={{fontSize:30}}>rajput</Text>
 <Text style={{fontSize:30}}>aurangabad</Text>
-<Button title='Click '></Button> 
+<Button title='Click '></Button>  */}
+
+ {/* we can write html in view component and we can write js inside html------- */}
+
+ <Text style={{fontSize:30}}>{name}</Text>
+  <Text style={{fontSize:30}}>{age}</Text>
+  <Text style={{fontSize:30}}>{email}</Text>
 
   
     
