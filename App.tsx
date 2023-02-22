1539f0e833f2ec0a3e074ca4317ae03d2fd30249
Mritunjay Kumar <akshaysingh906022@gmail.com>
2023-02-23 01:47:59 +0530

    add components

--- App.tsx
+++ App.tsx
@@ -29,16 +29,42 @@ function App(): JSX.Element {
 
  {/* we can write html in view component and we can write js inside html------- */}
 
- <Text style={{fontSize:30}}>{name}</Text>
+ {/* <Text style={{fontSize:30}}>{name}</Text>
   <Text style={{fontSize:30}}>{age}</Text>
-  <Text style={{fontSize:30}}>{email}</Text>
+  <Text style={{fontSize:30}}>{email}</Text> */}
+
+
+
+   {/* THIS IS HOW WE CAN USE MULTIPLE COMPONENTS  */}
+
+   <Text style={{fontSize:30}}>Components</Text>
+   <Component/>
+   <MyData/>
 
   
-    
-    </View>
-    
-    
+  </View>
+  
   );
+}
+
+
+const Component=()=>{
+  return (
+    <View>
+    <Text style={{fontSize:30}}>{name}</Text>
+      <Text style={{fontSize:10}}>singh</Text>
+      <Text style={{fontSize:30}}>singh</Text>
+    </View>
+  )
+}
+const MyData=()=>{
+  return (
+    <View>
+    <Text style={{fontSize:30}}>{name}</Text>
+      <Text style={{fontSize:10}}>20</Text>
+      <Text style={{fontSize:30}}>AURANGABAD</Text>
+    </View>
+  )
 }
 
 
