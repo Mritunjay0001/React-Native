151f5570e8bdf118b7b435acd4924d25ac6ac4c1
Mritunjay Kumar <akshaysingh906022@gmail.com>
2023-02-23 01:31:09 +0530

    native start

---
+++ App.tsx
@@ -0,0 +1,40 @@
+
+import React from 'react';
+// import type {PropsWithChildren} from 'react';
+import {
+
+  Text,
+ Button,
+  View,
+} from 'react-native';
+
+
+const name= "akshay";
+let age= 20;
+var email= "akshay@gmail.com";
+
+
+function App(): JSX.Element {
+  
+  return (
+    <View>
+      {/* this is the first thing ---------> */}
+
+
+<Text style={{fontSize:30}}>akshay</Text>
+<Text style={{fontSize:30}}>singh</Text>
+<Text style={{fontSize:30}}>rajput</Text>
+<Text style={{fontSize:30}}>aurangabad</Text>
+<Button title='Click '></Button> 
+
+  
+    
+    </View>
+    
+    
+  );
+}
+
+
+
+export default App;
