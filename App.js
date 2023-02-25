dc5454f81c49bc4228146efade5de7441b3f35e4
Mritunjay Kumar <akshaysingh906022@gmail.com>
2023-02-25 23:38:54 +0530

    class components

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 
-import React, { useState } from 'react';
+import React, { useState,Component } from 'react';
 
 // import type {PropsWithChildren} from 'react';
-import {Button,Text,View,StyleSheet, TextInput, FlatList} from 'react-native';
+import {Button,Text,View,StyleSheet, TextInput, FlatList, SectionList} from 'react-native';
 import MyData from './components/MyData';
+import Student from './components/Student';
 
 
 // const name= "akshay";
@@ -15,225 +16,271 @@ import MyData from './components/MyData';
 // }
 
 
-function App() {
-  // const [name,setName]=useState("Akshay Chauhan")
-  // const [name,setName]=useState("")
-  // const [email,setEmail]=useState("")
-  // const [password,setPassword]=useState("")
-  // const [display, setDisplay]=useState(false);
+// function App() {
+//   // const [name,setName]=useState("Akshay Chauhan")
+//   // const [name,setName]=useState("")
+//   // const [email,setEmail]=useState("")
+//   // const [password,setPassword]=useState("")
+//   // const [display, setDisplay]=useState(false);
 
-  // const resetData = () =>{
-  //   setDisplay(false);
-  //   setEmail(false);
-  //   setName(false);
-  //   setPassword(false)
-  // }
+//   // const resetData = () =>{
+//   //   setDisplay(false);
+//   //   setEmail(false);
+//   //   setName(false);
+//   //   setPassword(false)
+//   // }
 
-  // function updateIt(){
-  //   setName("rajput")
-  // }
+//   // function updateIt(){
+//   //   setName("rajput")
+//   // }
 
-  const users= [
-    {
-      id:1,
-      name:"Akshay",
-      email:"akshay@gmail.com",
-    },
-    {
-      id:2,
-      name:"Santosh",
-      email:"santosh@gmail.com"
-    },
-    {
-      id:3,
-      name:"Govind",
-      email:"govind@gmail.com"
-    },
-    {
-      id:4,
-      name:"Satya",
-      email:"satya@gmail.com"
-    }
-  ]
+//   // const users= [
+//   //   {
+//   //     id:1,
+//   //     name:"Akshay",
+//   //     email:"akshay@gmail.com",
+//   //   },
+//   //   {
+//   //     id:2,
+//   //     name:"Santosh",
+//   //     email:"santosh@gmail.com"
+//   //   },
+//   //   {
+//   //     id:3,
+//   //     name:"Govind",
+//   //     email:"govind@gmail.com"
+//   //   },
+//   //   {
+//   //     id:4,
+//   //     name:"Satya",
+//   //     email:"satya@gmail.com"
+//   //   }
+//   // ]
+
+//   // const users= [
+//   //   {
+//   //     id:1,
+//   //     name:"Akshay",
+//   //     emdata:["javascript","php","react","angular"]
+//   //   },
+//   //   {
+//   //     id:2,
+//   //     name:"Santosh",
+//   //     emdata:["C","php","C++","angular"]
+//   //   },
+//   //   {
+//   //     id:3,
+//   //     name:"Govind",
+//   //     edata:["Python","dsa","react","angular"]
+//   //   },
+//   //   {
+//   //     id:4,
+//   //     name:"Satya",
+//   //     data:["Native","php","react","comunication"]
+//   //   }
+//   // ]
   
-  return (
-//     <View>
-//       {/* this is the first thing ---------> */}
+//   return (
+// //     <View>
+// //       {/* this is the first thing ---------> */}
 
 
-// {/* <Text style={{fontSize:30}}>akshay</Text>
-// <Text style={{fontSize:30}}>singh</Text>
-// <Text style={{fontSize:30}}>rajput</Text>
-// <Text style={{fontSize:30}}>aurangabad</Text>
-// <Button title='Click '></Button>  */}
+// // {/* <Text style={{fontSize:30}}>akshay</Text>
+// // <Text style={{fontSize:30}}>singh</Text>
+// // <Text style={{fontSize:30}}>rajput</Text>
+// // <Text style={{fontSize:30}}>aurangabad</Text>
+// // <Button title='Click '></Button>  */}
 
-//  {/* we can write html in view component and we can write js inside html------- */}
+// //  {/* we can write html in view component and we can write js inside html------- */}
 
-//  {/* <Text style={{fontSize:30}}>{name}</Text>
-//   <Text style={{fontSize:30}}>{age}</Text>
-//   <Text style={{fontSize:30}}>{email}</Text> */}
+// //  {/* <Text style={{fontSize:30}}>{name}</Text>
+// //   <Text style={{fontSize:30}}>{age}</Text>
+// //   <Text style={{fontSize:30}}>{email}</Text> */}
 
 
 
-//    {/* THIS IS HOW WE CAN USE MULTIPLE COMPONENTS  */}
+// //    {/* THIS IS HOW WE CAN USE MULTIPLE COMPONENTS  */}
 
-//    {/* <Text style={{fontSize:30}}>Components</Text>
-//    <Some/>
-//    <MyData/> */}
+// //    {/* <Text style={{fontSize:30}}>Components</Text>
+// //    <Some/>
+// //    <MyData/> */}
 
 
   
 
   
-//   </View>
+// //   </View>
 
-<View>
-   {/* ------WE ARE GOING TO ADD BUTTON AND ALL------- */}
+// <View>
+//    {/* ------WE ARE GOING TO ADD BUTTON AND ALL------- */}
 
-   {/* <Text style={{fontSize:30}}>Button and OnPress</Text>
-   <Button title='Press Me' onPress={pressCheck} color={'black'}></Button> */}
+//    {/* <Text style={{fontSize:30}}>Button and OnPress</Text>
+//    <Button title='Press Me' onPress={pressCheck} color={'black'}></Button> */}
 
- {/* ------------USSTATE IN NATIVE AND UPDATE IN ONPRESS---- */}
+//  {/* ------------USSTATE IN NATIVE AND UPDATE IN ONPRESS---- */}
 
- {/* <Text style={{fontSize:30}}>Update The Name</Text>
- <Text style={{fontSize:20}}>{name}</Text> */}
+//  {/* <Text style={{fontSize:30}}>Update The Name</Text>
+//  <Text style={{fontSize:20}}>{name}</Text> */}
 
- {/* -------this is how we can set name and update name with parameter--- */}
- {/* <Button title='press' onPress={()=>setName("Sonu")}></Button> */}
+//  {/* -------this is how we can set name and update name with parameter--- */}
+//  {/* <Button title='press' onPress={()=>setName("Sonu")}></Button> */}
 
- {/* -----------this is how wer can update date with function call---- */}
-{/* <Button title='press' onPress={updateIt}></Button> */}
-
-
- {/* -------------PROPS and update IN NATIVE------ */}
-
- {/* <Text style={{fontSize:30}}>Props In Native</Text>
- <Button title='update' onPress={()=>setName("sonu rajput")}></Button>
- <PropsData name={name}/> */}
-
-  {/* -----------STYLING IN NATIVE------- */}
-               {/* Inline style */}
-  {/* <Text style={{fontSize:30, color:"red",marginTop:10, backgroundColor:"teal",padding:20,borderRadius:20, textAlign: 'center'}}>Styling In Native</Text> */}
-
-                 {/* Class components styling  */}
-    {/* <Text style={styles.textBox}>Wow </Text>        */}
+//  {/* -----------this is how wer can update date with function call---- */}
+// {/* <Button title='press' onPress={updateIt}></Button> */}
 
 
+//  {/* -------------PROPS and update IN NATIVE------ */}
 
-    {/* -------GET THE INPUT VALUE | HANDLING THE TEXT-------- */}
-    {/* <Text style={{fontSize:30, color:"white",marginTop:10, backgroundColor:"teal",padding:20,borderRadius:20, textAlign: 'center'}}>Get text input value</Text>
+//  {/* <Text style={{fontSize:30}}>Props In Native</Text>
+//  <Button title='update' onPress={()=>setName("sonu rajput")}></Button>
+//  <PropsData name={name}/> */}
 
-    <Text style={{fontSize:30, color:"red",marginTop:10, backgroundColor:"black",padding:20,borderRadius:20, textAlign: 'center'}}>Your Name IS : {name} </Text>
+//   {/* -----------STYLING IN NATIVE------- */}
+//                {/* Inline style */}
+//   {/* <Text style={{fontSize:30, color:"red",marginTop:10, backgroundColor:"teal",padding:20,borderRadius:20, textAlign: 'center'}}>Styling In Native</Text> */}
 
-    <TextInput
-    style={{fontSize:16, color:"blue", borderWidth:2,marginTop:10,}}
-    placeholder='Enter Your Input'
-    value={name}
-    onChangeText={(text)=>setName(text)}
-    />
-    <Button title='CLEAR NAME' onPress={()=>setName('')}/>
-                   */}
+//                  {/* Class components styling  */}
+//     {/* <Text style={styles.textBox}>Wow </Text>        */}
 
 
-            {/* ----------FORM IN NATIVE--------        */}
 
-    {/* <Text style={{fontSize:30, color:"white",marginTop:10, backgroundColor:"teal",padding:20,borderRadius:20, textAlign: 'center'}}>Simple Form</Text>       
-    <TextInput 
-    placeholder='Enter user Name'
-    style={styles.textInput }
-    onChangeText={(text)=>setName(text)}
-    value={name}
-    /> 
+//     {/* -------GET THE INPUT VALUE | HANDLING THE TEXT-------- */}
+//     {/* <Text style={{fontSize:30, color:"white",marginTop:10, backgroundColor:"teal",padding:20,borderRadius:20, textAlign: 'center'}}>Get text input value</Text>
 
-<TextInput 
-    placeholder='Enter user Email'
-    style={styles.textInput }
-    onChangeText={(text)=>setEmail(text)}
-    value={email}
-    /> 
+//     <Text style={{fontSize:30, color:"red",marginTop:10, backgroundColor:"black",padding:20,borderRadius:20, textAlign: 'center'}}>Your Name IS : {name} </Text>
 
-<TextInput 
-    placeholder='Enter user Password'
-    style={styles.textInput }
-    onChangeText={(text)=>setPassword(text)}
-    secureTextEntry={true}
-    value={password}
-    /> 
-   <View style={{marginTop:10,marginBottom:10}}>
-   <Button color={"green"} title='Print detail' onPress={()=>setDisplay(true)} />
-   </View>
-   <Button color={"red"} title='clear detail'  onPress={()=>setDisplay(resetData)} />
+//     <TextInput
+//     style={{fontSize:16, color:"blue", borderWidth:2,marginTop:10,}}
+//     placeholder='Enter Your Input'
+//     value={name}
+//     onChangeText={(text)=>setName(text)}
+//     />
+//     <Button title='CLEAR NAME' onPress={()=>setName('')}/>
+//                    */}
 
-   <View>
-    {
-      display ?
-      <View>
-        <Text style={{fontSize:20}}>User Name Is : {name}</Text>
-        <Text style={{fontSize:20}}>User Email Is : {email}</Text>
-        <Text style={{fontSize:20}}>User Password Is : {password}</Text>
-      </View>:null
-    }
-   </View> */}
+
+//             {/* ----------FORM IN NATIVE--------        */}
+
+//     {/* <Text style={{fontSize:30, color:"white",marginTop:10, backgroundColor:"teal",padding:20,borderRadius:20, textAlign: 'center'}}>Simple Form</Text>       
+//     <TextInput 
+//     placeholder='Enter user Name'
+//     style={styles.textInput }
+//     onChangeText={(text)=>setName(text)}
+//     value={name}
+//     /> 
+
+// <TextInput 
+//     placeholder='Enter user Email'
+//     style={styles.textInput }
+//     onChangeText={(text)=>setEmail(text)}
+//     value={email}
+//     /> 
+
+// <TextInput 
+//     placeholder='Enter user Password'
+//     style={styles.textInput }
+//     onChangeText={(text)=>setPassword(text)}
+//     secureTextEntry={true}
+//     value={password}
+//     /> 
+//    <View style={{marginTop:10,marginBottom:10}}>
+//    <Button color={"green"} title='Print detail' onPress={()=>setDisplay(true)} />
+//    </View>
+//    <Button color={"red"} title='clear detail'  onPress={()=>setDisplay(resetData)} />
+
+//    <View>
+//     {
+//       display ?
+//       <View>
+//         <Text style={{fontSize:20}}>User Name Is : {name}</Text>
+//         <Text style={{fontSize:20}}>User Email Is : {email}</Text>
+//         <Text style={{fontSize:20}}>User Password Is : {password}</Text>
+//       </View>:null
+//     }
+//    </View> */}
 
       
-      {/* ---------------MAKING LIST IN NATIVE-------- */}
+//       {/* ---------------MAKING LIST IN NATIVE-------- */}
      
-      {/* <Text style={{fontSize:20, color:"white",marginTop:10, backgroundColor:"teal",padding:20,borderRadius:20, textAlign: 'center'}}>LIST WITH FLAT LIST COMPONENTS</Text> 
-      <FlatList 
-      data={users}
-      renderItem={({item})=> <Text>{item.name}</Text>}
-      /> */}
+//       {/* <Text style={{fontSize:20, color:"white",marginTop:10, backgroundColor:"teal",padding:20,borderRadius:20, textAlign: 'center'}}>LIST WITH FLAT LIST COMPONENTS</Text> 
+//       <FlatList 
+//       data={users}
+//       renderItem={({item})=> <Text>{item.name}</Text>}
+//       /> */}
 
 
-       {/* ---------HOW TO MAKE GRID---- */}
-       {/* <Text style={{fontSize:20, color:"white",marginTop:10, backgroundColor:"teal",padding:20,borderRadius:20, textAlign: 'center'}}>GRID WITH DYNAMIC DATA</Text> */}
+//        {/* ---------HOW TO MAKE GRID---- */}
+//        {/* <Text style={{fontSize:20, color:"white",marginTop:10, backgroundColor:"teal",padding:20,borderRadius:20, textAlign: 'center'}}>GRID WITH DYNAMIC DATA</Text> */}
 
-       {/* <View style={{flex:1,flexDirection:"row",flexWrap:"wrap"}}>
-        <Text style={styles.item}>AKSHAY</Text>
-        <Text style={styles.item}>AKSHAY</Text>
-        <Text style={styles.item}>AKSHAY</Text>    
-        <Text style={styles.item}>AKSHAY</Text>   
-       </View> */}
+//        {/* <View style={{flex:1,flexDirection:"row",flexWrap:"wrap"}}>
+//         <Text style={styles.item}>AKSHAY</Text>
+//         <Text style={styles.item}>AKSHAY</Text>
+//         <Text style={styles.item}>AKSHAY</Text>    
+//         <Text style={styles.item}>AKSHAY</Text>   
+//        </View> */}
 
 
-         {/* --------COMPONENTS IN LOOP WITH FLATLIST------  */}
+//          {/* --------COMPONENTS IN LOOP WITH FLATLIST------  */}
 
-         <Text style={{fontSize:20, color:"white",marginTop:10, backgroundColor:"teal",padding:20,borderRadius:20, textAlign: 'center'}}>COMPONENTS IN LOOP WITH FLATLIST</Text>
-         <FlatList 
-         data={users}
-         renderItem={({item})=><UserData item={item}/>}
-           
-         />
-</View>
+//          {/* <Text style={{fontSize:20, color:"white",marginTop:10, backgroundColor:"teal",padding:20,borderRadius:20, textAlign: 'center'}}>COMPONENTS IN LOOP WITH FLATLIST</Text>
+//          <FlatList 
+//          data={users}
+//          renderItem={({item})=><UserData item={item}/>}
+//          /> */}
+
+
   
-  );
-}
 
-const UserData=(props)=>{
-const item=props.item;
-  return (
-    <View style={styles.box}>
-    <Text style={styles.item}>{item.name}</Text>
-    <Text style={styles.item}>{item.email}</Text>
-        
-  </View>
-  )
-}
+// </View>
+  
+//   );
+// }
 
-const styles = StyleSheet.create({
-  item:{
-    fontSize:20,
-    color:"black",
-    flex:1,
-    margin:2
-  },
-  box:{
-flexDirection:"row",
-borderWidth:2,
-marginTop:10,
-padding:10 
+
+// ------------CLASS COMPONENTS IN NATIVE--------
+
+class App extends Component{
+  fruit =()=>{
+    console.warn("Please eat")
   }
-})
+  render(){
+    return (
+      <View>
+        <Text style={{fontSize:20,color:"black",textAlign:"center",backgroundColor:"grey",marginTop:10,padding:10,borderRadius:20}}>CLASS COMPONENTS</Text>
+        <TextInput placeholder='ENTER SOMETHING' />
+        <Button title='PRESS ME' onPress={this.fruit}></Button>
+        <Student />
+      </View>
+    )
+  }
+}
+
+
+// const UserData=(props)=>{
+// const item=props.item;
+//   return (
+//     <View style={styles.box}>
+//     <Text style={styles.item}>{item.name}</Text>
+//     <Text style={styles.item}>{item.email}</Text>
+        
+//   </View>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//   item:{
+//     fontSize:20,
+//     color:"black",
+//     flex:1,
+//     margin:2
+//   },
+//   box:{
+// flexDirection:"row",
+// borderWidth:2,
+// marginTop:10,
+// padding:10 
+//   }
+// })
 
 
 // const styles = StyleSheet.create({
---
+++ components/Student.js
@@ -0,0 +1,15 @@
+import React, { useState,Component } from 'react';
+import {Button,Text,View,StyleSheet, TextInput, FlatList, SectionList} from 'react-native';
+
+class Student extends Component{
+    render(){
+      return (
+        <View>
+          <Text style={{fontSize:20,color:"black",textAlign:"center",backgroundColor:"grey",marginTop:10,padding:10,borderRadius:20}}>STUDENT COMPONENTS</Text>
+       
+        </View>
+      )
+    }
+  }
+
+  export default Student;
